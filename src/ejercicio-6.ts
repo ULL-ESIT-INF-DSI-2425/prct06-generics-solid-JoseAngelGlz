37bafc1f0ff5f1e7f535fd2d5790170b307b70bf
Jose Angel <alu0101564180@ull.edu.es>
2025-03-10 18:09:27 +0000

    Resto de ejercicios completados

---
+++ src/ejercicio-6.ts
@@ -0,0 +1,23 @@
+// interface Flyers {
+//   fly(): void;
+// }
+
+// interface Swimmers {
+//   swim(): void;
+// }
+
+// class Bird implements Flyers {
+//   fly(): void {
+//     console.log("Flying...");
+//   }
+// }
+
+// class Sparrow extends Bird {}
+
+// class SwimingBirds implements Swimmers {
+//   swim(): void {
+//     console.log("Swimming...");
+//   }
+// }
+
+// class Penguin extends SwimingBirds {}
